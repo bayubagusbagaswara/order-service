1e19027fe769bbb97a3e126905f5b245cc8b28f8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-10 22:36:14 +0700

    add service contract OrderService

---
+++ src/main/java/com/techie/microservices/orderservice/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.techie.microservices.orderservice.service;
+
+import com.techie.microservices.orderservice.dto.OrderRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public void placeOrder(OrderRequest request) {
+
+    }
+}
