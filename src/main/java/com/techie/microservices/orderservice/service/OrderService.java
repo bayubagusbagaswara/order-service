96b3c242533fe434bd97dab30c5a8342c9eac1fa
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-10 22:49:45 +0700

    update method placeOrder

--- src/main/java/com/techie/microservices/orderservice/service/OrderService.java
+++ src/main/java/com/techie/microservices/orderservice/service/OrderService.java
@@ -1,12 +1,27 @@
 package com.techie.microservices.orderservice.service;
 
 import com.techie.microservices.orderservice.dto.OrderRequest;
+import com.techie.microservices.orderservice.model.Order;
+import com.techie.microservices.orderservice.respository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class OrderService {
 
-    public void placeOrder(OrderRequest request) {
+    private final OrderRepository orderRepository;
 
+    public void placeOrder(OrderRequest request) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(request.price());
+        order.setSkuCode(request.skuCode());
+        order.setQuantity(request.quantity());
+
+        orderRepository.save(order);
     }
+
 }
