4e1b0472a1b0e946481abe6f803115e5dada9b05
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-16 15:00:21 +0700

    add url for inventory as a client

--- src/main/java/com/techie/microservices/orderservice/client/InventoryClient.java
+++ src/main/java/com/techie/microservices/orderservice/client/InventoryClient.java
@@ -1,4 +1,7 @@
 package com.techie.microservices.orderservice.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
 public class InventoryClient {
 }
