ab235e8a5e3671e144f28d58b329f58d35b88cbd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-06-18 17:22:55 +0700

    add unit test

--- src/test/java/com/techie/microservices/orderservice/TestOrderServiceApplication.java
+++ src/test/java/com/techie/microservices/orderservice/TestOrderServiceApplication.java
@@ -1,23 +1,39 @@
 package com.techie.microservices.orderservice;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration(proxyBeanMethods = false)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestOrderServiceApplication {
 
 	@Bean
 	@ServiceConnection
 	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+		MySQLContainer<?> mysql = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+//				.withDatabaseName("testdb")
+//				.withUsername("testuser")
+//				.withPassword("testpass");
+		mysql.start();
+		return mysql;
+	}
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.from(OrderServiceApplication::main).with(TestOrderServiceApplication.class).run(args);
 	}
-
 }
